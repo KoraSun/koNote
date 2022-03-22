acef3b56f86270009521eb320030e4f2ac02a58d
kora <bettm9706@gmail.com>
2022-03-22 12:26:57 +0800

    h获取数据动态展示

--- src/components/NotebookLists.vue
+++ src/components/NotebookLists.vue
@@ -1,32 +1,22 @@
 <template>
     <div id="notebook-list" class="detail">
         <header>
-            <a href="#" class="button"> <i class="iconfont icon-plus"> 新建笔记</i></a>
+            <a href="#" class="button" @click="onCreate"> <i class="iconfont icon-plus"> 新建笔记</i></a>
         </header>
         <main>
             <div class="layout">
-                <h3>笔记本列表(10)</h3>
+                <h3>笔记本列表({{notebooks.length}})</h3>
                 <div class="lists">
-                    <a href="#" class="notebook">
+                    <router-link v-for="notebook in notebooks" to='/notebooks/1' class="notebook" :key='notebook.id'>
                         <div>
                             <span class='iconfont icon-notebook'></span>
-                            笔记本标题1
-                            <span>3</span>
-                            <span class="action">编辑</span>
-                            <span class="action">删除</span>
-                            <span class="date">3天前</span>
+                            {{notebook.title}}
+                            <span>{{notebook.noteCounts}}</span>
+                            <span class="action" @click.stop.prevent="onEdit(notebook)">编辑</span>
+                            <span class="action" @click.stop.prevent="onDelete(notebook)">删除</span>
+                            <span class="date">{{notebook.createdAt}}</span>
                         </div>
-                    </a>
-                    <a href="#" class="notebook">
-                        <div>
-                            <span class="iconfont icon-notebook"></span>
-                            笔记本标题2
-                            <span>1</span>
-                            <span class="action">编辑</span>
-                            <span class="action">删除</span>
-                            <span class="date">5天前</span>
-                        </div>
-                    </a>
+                    </router-link>
                 </div>
             </div>
         </main>
@@ -37,10 +27,12 @@
 import Auth from '@/apis/auth'
 import Notebooks from '@/apis/notebooks'
 
-window.Notebooks= Notebooks
+//window.Notebooks= Notebooks
     export default {
         data(){
-            return {message:'笔记本列表'}
+            return {
+               notebooks:[]
+            }
 
         },
         created(){
@@ -50,7 +42,48 @@ window.Notebooks= Notebooks
                     this.$router.push('/login')
                 }
             })
+            Notebooks.getAll().then(res=>{
+                this.notebooks=res.data
+            })
         },
+        methods:{
+            onCreate(){
+                let title = window.prompt('创建笔记本')
+                if (title.trim() === ''){
+                    alert('标题不能为空')
+                    return 
+                }
+                console.log('create..')
+                Notebooks.addNotebook({title}).then(
+                    res=>{
+                        this.notebooks.unshift(res.data)
+                    }
+                )   
+            },
+            onEdit(notebook){
+                console.log('edit..',notebook)
+                let title =window.prompt('编辑标题')
+                Notebooks.updateNoteBook(notebook.id,{title})
+                .then(
+                    res=>{
+                    console.log(res)
+                    notebook.title=title
+                    }
+                )
+            },
+            onDelete(notebook){
+                console.log('delete..',notebook)
+                let isConfirm = window.confirm('确定要删除吗')
+                if(isConfirm){
+                    Notebooks.deleteNotebook(notebook.id)
+                    .then(
+                        res=>{
+                            console.log(res)
+                            this.notebooks.splice(this.notebooks.indexOf(notebook),1)
+                        })     
+                }
+            }
+        }
         
     }
 </script>
