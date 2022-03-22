1a3829bab7e70037d865a7a3617bd31ac84484ce
kora <bettm9706@gmail.com>
2022-03-22 11:04:40 +0800

    笔记本列表API封装

--- src/components/NotebookLists.vue
+++ src/components/NotebookLists.vue
@@ -1,22 +1,43 @@
 <template>
-    <div id="notebook-list">
-        <h1>
-            {{message}}
-        </h1>
-        <ul>
-            <li>
-                <router-link to="/notebooks/1">笔记本1</router-link>
-            </li>
-            <li>
-                <router-link to="/notebooks/2">笔记本2</router-link>
-            </li>
-        </ul>
-     
+    <div id="notebook-list" class="detail">
+        <header>
+            <a href="#" class="button"> <i class="iconfont icon-plus"> 新建笔记</i></a>
+        </header>
+        <main>
+            <div class="layout">
+                <h3>笔记本列表(10)</h3>
+                <div class="lists">
+                    <a href="#" class="notebook">
+                        <div>
+                            <span class='iconfont icon-notebook'></span>
+                            笔记本标题1
+                            <span>3</span>
+                            <span class="action">编辑</span>
+                            <span class="action">删除</span>
+                            <span class="date">3天前</span>
+                        </div>
+                    </a>
+                    <a href="#" class="notebook">
+                        <div>
+                            <span class="iconfont icon-notebook"></span>
+                            笔记本标题2
+                            <span>1</span>
+                            <span class="action">编辑</span>
+                            <span class="action">删除</span>
+                            <span class="date">5天前</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </main>
     </div>
 </template>
 
 <script>
 import Auth from '@/apis/auth'
+import Notebooks from '@/apis/notebooks'
+
+window.Notebooks= Notebooks
     export default {
         data(){
             return {message:'笔记本列表'}
@@ -34,9 +55,7 @@ import Auth from '@/apis/auth'
     }
 </script>
 
-<style  scoped>
-h1{
-    color: blue;
-}
+<style lang='less' scoped>
+@import url(../assets/css/notebookLists.less);
 
 </style>
